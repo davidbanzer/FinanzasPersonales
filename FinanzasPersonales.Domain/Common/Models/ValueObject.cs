dd31acf2df321527a86f0768ef8f2a270a6adadc
David Banzer <davidbanzer123@gmail.com>
2023-09-26 16:12:55 -0400

    Agregar base VO, AR y Entity

---
+++ FinanzasPersonales.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,58 @@
+namespace FinanzasPersonales.Domain.Common.Models;
+
+
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+  public abstract IEnumerable<object> GetEqualityComponents();
+
+  public override bool Equals(object? obj)
+  {
+    if (obj is null || obj.GetType() != GetType())
+    {
+      return false;
+    }
+
+    var valueObject = (ValueObject)obj;
+
+    return GetEqualityComponents().SequenceEqual(valueObject.GetEqualityComponents());
+  }
+
+  public static bool operator ==(ValueObject? left, ValueObject? right)
+  {
+    return Equals(left, right);
+  }
+
+  public static bool operator !=(ValueObject? left, ValueObject? right)
+  {
+    return !Equals(left, right);
+  }
+
+  public override int GetHashCode()
+  {
+    return GetEqualityComponents()
+      .Select(x => x?.getHashCode() ?? 0)
+      .Aggregate((x, y) => x ^ y);
+  }
+
+  public bool Equals(ValueObject? other)
+  {
+    return Equals((object?)other);
+  }
+}
+
+
+public class Ammount : ValueObject
+{
+    public decimal Value { get; set; }
+
+    public Ammount(decimal value)
+    {
+        Value = value;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
